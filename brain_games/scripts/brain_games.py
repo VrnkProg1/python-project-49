5eea7dd8ec6836968f0ca9da5b4b6f75d044598e
nika <khitrozhopyy_ananasik@mail.ru>
2023-01-24 17:05:56 +0300

    add .

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+import prompt
+
 def main():
     print('Welcome to the Brain Games')
+    print('May I have your name? ', end='')
+    name = input()
+    name = prompt.string('May I have your name?')
+    print('Hello,', name, '!')
 
 
 if __name__ == '__main__':
