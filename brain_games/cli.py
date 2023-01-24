287744238ccd7442a0f9d175de3196e72f78d7df
nika <khitrozhopyy_ananasik@mail.ru>
2023-01-24 17:37:47 +0300

    add .

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+def welcome_user:
+    print('Welcome to the Brain Games!')
+    print('May I have your name?')
+    name = input()
+    name = prompt.string('May I have your name? ')
+    print('Hello', name, '!')
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,8 +4,6 @@ import prompt
 
 def main():
     print('Welcome to the Brain Games')
-    print('May I have your name? ', end='')
-    name = input()
     name = prompt.string('May I have your name?')
     print('Hello,', name, '!')
 
