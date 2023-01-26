c9c0f12048c7891567c32ef75729430b70b59e86
nika <khitrozhopyy_ananasik@mail.ru>
2023-01-26 15:02:37 +0300

    add .

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -10,18 +10,26 @@ def main():
     print('Hello, ', name + '!')
     print('What is the result of the expression?')
 
-    count += 1
-    while count < 3:1
+    count = 0
+    while count < 3:
         list = ['+', '-', '*']
         value1 = random.randint(1, 100)
         value2 = random.randint(1, 100)
-        operator = random.choise(list)
-        value = value1, operator, value2
-        print('Question: ', value)
+        operator = random.sample(list, 1)
+        if operator == ['+']:
+            value = value1 + value2
+            question1 = f'{value1} {"+"} {value2}'
+        elif operator == ['-']:
+            value = value1 - value2
+            question1 = f'{value1} {"-"} {value2}'
+        else:
+            value = value1 * value2
+            question1 = f'{value1} {"*"} {value2}'
+        print('Question: ', question1)
         answer = prompt.string('Your answer: ')
         if answer == value:
             print('Correct!')
-        else:
+        elif answer != value:
             print(f"'{answer}'", 'is wrong answer ;(. Correct answer was ', f"'{value}'")
             print("Let's try again, " + name + '!')
             break
