2f0b173a47258a5b167493cf647adb9630193f89
nika <khitrozhopyy_ananasik@mail.ru>
2023-01-29 12:36:11 +0300

    add .

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,30 +1,22 @@
-def brain_calc():
-    import random
+import random
 
-    count = 0
-    while count < 3:
+def rules():
+    print('What is the result of the expression?')
+
+
+def calc():
         list = ['+', '-', '*']
         value1 = random.randint(1, 100)
         value2 = random.randint(1, 100)
         operator = random.sample(list, 1)
         if operator == ['+']:
-            value = value1 + value2
-            question1 = f'{value1} {"+"} {value2}'
+            val = value1 + value2
+            question = f'{value1} {"+"} {value2}'
         elif operator == ['-']:
-            value = value1 - value2
-            question1 = f'{value1} {"-"} {value2}'
+            val = value1 - value2
+            question = f'{value1} {"-"} {value2}'
         else:
-            value = value1 * value2
-            question1 = f'{value1} {"*"} {value2}'
-        print('Question: ', question1)
-        answer = prompt.string('Your answer: ')
-        if int(answer) == value:
-            print('Correct!')
-        elif int(answer) != value:
-            print(f"'{answer}'", 'is wrong answer ;(. Correct answer was ', f"'{value}'")
-            print("Let's try again, " + name + '!')
-            break
-        count += 1
-        if count == 3:
-            print('Congratulations, ' + name + '!')
-
+            val = value1 * value2
+            question = f'{value1} {"*"} {value2}'
+        print('Question: ', question)
+        return val
