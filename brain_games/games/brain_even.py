40f4fb71421bdb539c104157b524309fa2c6cf31
nika <khitrozhopyy_ananasik@mail.ru>
2023-02-09 10:56:13 +0300

    add brain-even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -4,12 +4,11 @@ from random import randint
 def rules():
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
-
+    
 def even():
-    a = randint(1, 100)
-    if a % 2 == 0:
-        val = 'yes'
-    elif a % 2 != 0:
-        val = 'no'
-    print('Question:', a)
-    return val
+    number = randint(1, 100)
+    if number % 2 == 0:
+        return True
+    elif number % 2 != 0:
+        return False
+    
