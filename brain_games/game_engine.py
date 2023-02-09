93de32f5b4d5932cdfc5c20a10e12aa14f76b27c
nika <khitrozhopyy_ananasik@mail.ru>
2023-02-09 12:06:02 +0300

    add game_engine.py

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -8,17 +8,17 @@ def welcome_user(rulesarg, game):
     count = 0
     rulesarg()
     while count < 3:
-        value = game()
-        print(game())
-        if value == False:
+        correct, question, number = game()
+        print(question, number)
+        if correct == False:
             expected_answer = 'no'
-        elif value == True:
+        elif correct == True:
             expected_answer = 'yes'
         answer = prompt.string('Your answer: ')
-        if (value == True and answer == 'yes') or (value == False and answer == 'no'):
+        if (correct == True and answer == 'yes') or (correct == False and answer == 'no'):
             print('Correct!')
             count += 1
-        elif (value == False and answer == 'yes') or (value == True and answer == 'no'):
+        elif (correct == False and answer == 'yes') or (correct == True and answer == 'no'):
             wrong = 'is wrong answer ;(. Correct answer was'
             print(f"'{answer}' {wrong} '{expected_answer}'")
             print("Let's try again, " + name + '!')
