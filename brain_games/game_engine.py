15b0ff4863020ae134ef2c93b24f344c9b8f879c
nika <khitrozhopyy_ananasik@mail.ru>
2023-02-09 10:57:24 +0300

    add .

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,21 +1,26 @@
 import prompt
 
 
-def welcome_user(rulsearg, game):
+def welcome_user(rulesarg, game):
     print('Welcome to the Brain Games! ')
     name = prompt.string('May I have your name? ')
     print('Hello,', name + '!')
     count = 0
-    rulsearg()
+    rulesarg()
     while count < 3:
         value = game()
+        print(game())
+        if value == False:
+            expected_answer = 'no'
+        elif value == True:
+            expected_answer = 'yes'
         answer = prompt.string('Your answer: ')
-        wrong = 'is wrong answer ;(. Correct answer was'
-        if answer == str(value):
+        if (value == True and answer == 'yes') or (value == False and answer == 'no'):
             print('Correct!')
             count += 1
-        elif answer != value:
-            print(f"'{answer}' {wrong} '{value}'")
+        elif (value == False and answer == 'yes') or (value == True and answer == 'no'):
+            wrong = 'is wrong answer ;(. Correct answer was'
+            print(f"'{answer}' {wrong} '{expected_answer}'")
             print("Let's try again, " + name + '!')
             break
         if count == 3:
--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,13 +5,18 @@ def rules():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
 
-def prime():
-    k = random.randint(2, 100)
-    for i in range(2, int(k / 2) + 1):
-        if (k % i) == 0:
-            val = 'no'
-            break
+def check():
+    number = random.randint(2, 100)
+    for i in range(2, int(number / 2) + 1):
+        if (number % i) == 0:
+            print('Question:', number)
+            correct = False
+            return correct
         else:
-            val = 'yes'
-    print('Question:', k)
-    return val
+            print('Question:', number)
+            correct = True
+            return correct
+    
+    
+def prime():
+    check()
