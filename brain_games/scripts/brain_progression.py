883c49da89c4c196414ef2602a53d3a1b1031f1a
nika <khitrozhopyy_ananasik@mail.ru>
2023-02-11 17:29:45 +0300

    All at once

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from brain_games.game_engine import welcome_user
+from brain_games.game_engine import hull_of_game
 from brain_games.games.brain_progression import progression
 from brain_games.games.brain_progression import rules
 
 
 def main():
-    welcome_user(rules, progression)
+    hull_of_game(rules, progression)
 
 
 if __name__ == '__main__':
