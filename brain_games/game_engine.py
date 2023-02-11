da89c308a314aa9a71cc4369c52418b40de30a10
nika <khitrozhopyy_ananasik@mail.ru>
2023-02-11 15:11:15 +0300

    add brain_games/games/brain_calc.py

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -8,17 +8,27 @@ def welcome_user(rulesarg, game):
     count = 0
     rulesarg()
     while count < 3:
-        correct, question, number = game()
-        print(question, number)
-        if correct == 'False':
-            expected_answer = 'no'
-        elif correct == 'True':
+        correct, question, expression = game()
+        print(question, expression)
+        if correct == 'True':
             expected_answer = 'yes'
+        elif correct == 'False':
+            expected_answer = 'no'
+        if correct == int():
+            expected_answer = correct
         answer = prompt.string('Your answer: ')
         if (correct == 'True' and answer == 'yes') or (correct == 'False' and answer == 'no'):
             print('Correct!')
             count += 1
         elif (correct == 'False' and answer == 'yes') or (correct == 'True' and answer == 'no'):
+            wrong = 'is wrong answer ;(. Correct answer was'
+            print(f"'{answer}' {wrong} '{expected_answer}'")
+            print("Let's try again, " + name + '!')
+            break
+        if correct == int() and correct == answer:
+            print('Correct!')
+            count += 1
+        elif correct == int() and correct != answer:
             wrong = 'is wrong answer ;(. Correct answer was'
             print(f"'{answer}' {wrong} '{expected_answer}'")
             print("Let's try again, " + name + '!')
--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -5,19 +5,23 @@ def rules():
     print('What is the result of the expression?')
 
 
-def calc():
-    list = ['+', '-', '*']
+def check():
+    list_of_operands = ['+', '-', '*']
     value1 = random.randint(1, 100)
     value2 = random.randint(1, 100)
-    operator = random.sample(list, 1)
+    operator = random.sample(list_of_operands, 1)
     if operator == ['+']:
-        val = value1 + value2
-        question = f'{value1} {"+"} {value2}'
+        expected_answer = value1 + value2
+        expression = f'{value1} {"+"} {value2}'
     elif operator == ['-']:
-        val = value1 - value2
-        question = f'{value1} {"-"} {value2}'
+        expected_answer = value1 - value2
+        expression = f'{value1} {"-"} {value2}'
     else:
-        val = value1 * value2
-        question = f'{value1} {"*"} {value2}'
-    print('Question:', question)
-    return val
+        expected_answer = value1 * value2
+        expression = f'{value1} {"*"} {value2}'
+    question = 'Question:'
+    return expected_answer, question, expression
+
+
+def calc():
+    check()
