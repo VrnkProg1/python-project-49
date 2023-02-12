e5da497f1281b193ac126ba58b64d10edd88b7c2
nika <khitrozhopyy_ananasik@mail.ru>
2023-02-12 13:49:08 +0300

    add brain_games/games/brain_gcd.py

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -6,14 +6,14 @@ def rules():
 
 
 def gcd():
-    a = randint(1, 50)
-    b = randint(1, 50)
+    first_number = randint(1, 50)
+    second_number = randint(1, 50)
     question = 'Question:'
-    expression = f'{a} {b}'
-    while a != 0 and b != 0:
-        if a > b:
-            a = a % b
+    expression = f'{first_number} {second_number}'
+    while first_number != 0 and second_number != 0:
+        if first_number > second_number:
+            first_number = first_number % second_number
         else:
-            b = b % a
-    correct = a + b
+            second_number = second_number % first_number
+    correct = first_number + second_number
     return correct, question, expression
