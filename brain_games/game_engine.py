049a92377674d15e3c06599971280507480da380
Veronika Vlasova <vlasova_nika69@mail.ru>
2023-02-16 13:07:55 +0300

    add brain_games/game_engine.py

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,7 +1,7 @@
 import prompt
 
 
-def hull_of_game(rulesarg, game):
+def hull_of_game(rulesarg, game):  # noqa: C901
     print('Welcome to the Brain Games! ')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
