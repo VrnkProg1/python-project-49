a31476095b93c44590b07c69e817c2d5eace2364
Veronika Vlasova <vlasova_nika69@mail.ru>
2023-02-19 15:12:33 +0300

    add brain_games/game_engine.py

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -5,9 +5,9 @@ def hull_of_game(rulesarg, game):
     print('Welcome to the Brain Games! ')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    count = 0
     rules = rulesarg()
     print(rules)
+    count = 0
     while count < 3:
         correct, question, expression = game()
         print(question, expression)
